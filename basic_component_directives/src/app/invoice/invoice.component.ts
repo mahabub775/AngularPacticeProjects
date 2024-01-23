dd7fa4b201302a2dcce24a5d95073e3be72b6989
Mahabub <mahabub775@gmail.com>
2024-01-23 08:41:04 +0600

    delete_actions

--- basic_component_directives/src/app/invoice/invoice.component.ts
+++ basic_component_directives/src/app/invoice/invoice.component.ts
@@ -1,27 +1,54 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { FormGroup, FormControl, ReactiveFormsModule, Validators  } from '@angular/forms';
+
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [CommonModule,  ReactiveFormsModule],
+  imports: [CommonModule,  ReactiveFormsModule ],
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.css'
 })
 export class InvoiceComponent {
 
+
   invoicecform = new FormGroup({
-    customerName : new FormControl(''),
-    productName : new FormControl(''),
-    price : new FormControl(0)
+    id : new FormControl(0),
+    customerName : new FormControl('', Validators.required),
+    productName : new FormControl('', Validators.required),
+    price : new FormControl(0, Validators.required)
 
   })
+  Invoices:any =[]
+  constructor(){
+     this.Invoices = [{id:1, customerName:'Customer-A', productName:'Mobile',price: 1500}];  
+  }
   submitInvoice(){
-    let tempinvoice = { customerName: this.invoicecform.value.customerName,
+    let tempinvoice = { 
+      id:this.Invoices.length+1,
+      customerName: this.invoicecform.value.customerName,
       productName: this.invoicecform.value.productName,
       price: this.invoicecform.value.price
     }
-    console.log(tempinvoice);
+    //console.log(tempinvoice);
+    this.Invoices.push(tempinvoice);
+    this.Reset();
+  }
+  Delete(obj:any){
+
+    var otempObjs =  this.Invoices.filter( (element: any) => element != obj);
+    this.Invoices = otempObjs;
+
+  }
+  Reset(){
+    this.invoicecform = new FormGroup({
+      id : new FormControl(0),
+      customerName : new FormControl(''),
+      productName : new FormControl(''),
+      price : new FormControl(0)
+  
+    })
   }
 }
